6c348565ab0b97529f0802abc659dbc114af5ea7
alvina <alvina@qburst.com>
2024-10-22 06:04:34 +0000

    updated to use deque for maintaining the history

--- BallTracking/TrackedObjectClass.py
+++ BallTracking/TrackedObjectClass.py
@@ -1,27 +1,25 @@
 import math
+from collections import deque
 
 class TrackedObject:
     def __init__(self, objectID, centroid, maxHistory=5):
         # Assign an ID to the object and initialize properties
         self.objectID = objectID
         self.centroid = centroid
-        self.centroid_history = [centroid]
+        self.centroid_history = deque([centroid], maxlen=maxHistory)  # Using deque for centroid history
         self.maxHistory = maxHistory
         self.disappeared = 0
         self.average_centroid = centroid
         self.displacement = 0
         self.direction = 0
         self.vector = (0, 0)
-        self.vector_history = [(0, 0)]  # Store previous vectors
-        self.displacement_history = [0]
+        self.vector_history = deque([(0, 0)], maxlen=maxHistory)  # Using deque for vector history
+        self.displacement_history = deque([0], maxlen=maxHistory)  # Using deque for displacement history
 
     def update_centroid(self, new_centroid):
         # Update the current centroid and maintain centroid history
-        # update movement before new value is updated to history
         self.update_movements(new_centroid)
         self.centroid_history.append(new_centroid)
-        if len(self.centroid_history) > self.maxHistory:
-            self.centroid_history.pop(0)
 
     def mark_disappeared(self):
         # Increase the disappeared count for the object
@@ -41,29 +39,23 @@ class TrackedObject:
     def update_centroid_average(self):
         # Compute the average of the centroid history
         self.average_centroid = tuple(map(lambda x: int(sum(x) / len(x)), zip(*self.centroid_history)))
-    
+
     def update_displacement(self):
-        delta_x,delta_y = self.vector
-        self.displacement = math.sqrt(delta_x**2+delta_y**2)
-        self.displacement_history.append(self.displacement )
-        if len(self.displacement_history) > self.maxHistory:
-            self.displacement_history.pop(0)
+        delta_x, delta_y = self.vector
+        self.displacement = (delta_x**2 + delta_y**2)**0.5
+        self.displacement_history.append(self.displacement)
 
     def update_vector(self, new_centroid):
         # Calculate the x and y components of the vector
         vector_x = new_centroid[0] - self.average_centroid[0]
         vector_y = new_centroid[1] - self.average_centroid[1]
         self.vector = (vector_x, vector_y)
-        # Add the new vector to the vector history
+        # Add the new vector to the vector history and smooth it
         self.vector_history.append(self.vector)
-        # Calculate average vector before new vector is added to history
         self.smooth_vector()
-        if len(self.vector_history) > self.maxHistory:
-            self.vector_history.pop(0)
 
     def smooth_vector(self):
         # Compute the average vector from vector history for smoothing
         avg_vector_x = sum(v[0] for v in self.vector_history) / len(self.vector_history)
         avg_vector_y = sum(v[1] for v in self.vector_history) / len(self.vector_history)
-        # Set the smoothed vector
-        self.vector = (avg_vector_x, avg_vector_y)
+        self.vector = (avg_vector_x, avg_vector_y)  # Set the smoothed vector
